2bf26db6b2f4e888aee903e9b6c8d43a19c9c915
Piotr Szul <piotr.szul@gmail.com>
2016-08-05 16:30:45 +1000

    Updated sample

--- scripts/run-importance-ch22.sh
+++ scripts/run-importance-ch22.sh
@@ -3,4 +3,4 @@
 PWD=$(cd `dirname "$0"`/..; pwd)
 PATH=${PATH}:${PWD}
 
-variant-spark --spark --master yarn-client --num-executors 32 --executor-memory 4G --driver-memory 4G -- importance -if /flush2/szu004/variant-spark/ALL.chr22.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf -v -t 20
+variant-spark --spark --master yarn-client --num-executors 32 --executor-memory 4G --driver-memory 4G -- importance -if /flush2/szu004/variant-spark/ALL.chr22.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf -ff data/small-labels.csv -fc 22_16051249 -v -t 20
