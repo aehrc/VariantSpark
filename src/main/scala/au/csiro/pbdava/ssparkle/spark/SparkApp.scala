c0cfe92a3f61abee059dc3775d3c67deac753d16
Piotr Szul <piotr.szul@csiro.au>
2016-08-05 14:17:02 +1000

    Fixed config logging

--- src/main/scala/au/csiro/pbdava/ssparkle/spark/SparkApp.scala
+++ src/main/scala/au/csiro/pbdava/ssparkle/spark/SparkApp.scala
@@ -3,23 +3,27 @@ package au.csiro.pbdava.ssparkle.spark
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.Logging
+
 //import org.bdgenomics.adam.rdd.ADAMContext
 
-trait SparkApp {
+trait SparkApp extends Logging {
   def defaultMasterUrl = "local"
   Logger.getLogger("org").setLevel(Level.OFF)
   Logger.getLogger("akka").setLevel(Level.OFF)
   
-  lazy val conf = new SparkConf()
-    .setMaster(masterUrl)
-    .setAppName(getClass().getSimpleName)
+  lazy val conf = { 
+      val conf = new SparkConf().setAppName(getClass().getSimpleName)
+      if (conf.contains("spark.master")) conf else conf.setMaster(defaultMasterUrl) 
+    } 
     //.set("spark.shuffle.memoryFraction", "0.2")
     //.set("spark.storage.memoryFraction", "0.4")
     //.set("spark.yarn.executor.memoryOverhead", "2048")
-    .set("spark.driver.maxResultSize","2048")
+    //.set("spark.driver.maxResultSize","2048")
     //.set("spark.default.parallelism", "256")
-  lazy val masterUrl =  if (System.getenv("MASTER") != null) System.getenv("MASTER") else defaultMasterUrl
-  lazy val sc = { println("MASTER: " + masterUrl) ; new SparkContext(conf)}
+  lazy val sc = {
+    logDebug("Spark conf: " + conf.toDebugString)
+    new SparkContext(conf)
+  }
   lazy val sqlContext = new SQLContext(sc)
-  //lazy val ac = new ADAMContext(sc)
 }
