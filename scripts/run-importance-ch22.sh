e46f26de94b80cefed1c9f0da9e4530c79493564
Piotr Szul <piotr.szul@gmail.com>
2016-08-08 13:42:00 +1000

    Changed samples to run on the test data

--- scripts/run-importance-ch22.sh
+++ scripts/run-importance-ch22.sh
@@ -3,4 +3,8 @@
 PWD=$(cd `dirname "$0"`/..; pwd)
 PATH=${PATH}:${PWD}
 
-variant-spark --spark --master yarn-client --num-executors 32 --executor-memory 4G --driver-memory 4G -- importance -if /flush2/szu004/variant-spark/ALL.chr22.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf -ff data/small-labels.csv -fc 22_16051249 -v -t 20
+DATA_ROOT=${VS_DATA_DIR:-.}
+DATA_DIR=${DATA_ROOT}/variant-spark-data
+DATA_INPUT_DIR=${DATA_DIR}/input
+
+variant-spark --spark --master yarn-client --num-executors 32 --executor-memory 4G --driver-memory 4G -- importance -if "${DATA_INPUT_DIR}/ALL.chr22.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.bz2" -ff "${DATA_INPUT_DIR}/small-labels.csv" -fc 22_16051249 -v -t 20
