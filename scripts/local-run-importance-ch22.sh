e2c0595e18aa26a4dee3f812a333a33b8ac25dde
Piotr Szul <piotr.szul@csiro.au>
2016-08-11 09:56:24 +1000

    Added printing command line when running examples

--- scripts/local-run-importance-ch22.sh
+++ scripts/local-run-importance-ch22.sh
@@ -2,6 +2,7 @@
 
 PWD=$(cd `dirname "$0"`/..; pwd)
 PATH=${PATH}:${PWD}
+export VS_ECHO_CMDLINE=YES
 
 variant-spark --local  --\
  importance -if ${PWD}/data/small.vcf -ff ${PWD}/data/small-labels.csv -fc 22_16051249 -v -t 20
