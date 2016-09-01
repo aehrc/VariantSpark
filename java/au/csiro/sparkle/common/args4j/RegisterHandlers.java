c3b4f589a0ccabf5e2ef72778999f8acddb156d8
Piotr Szul <piotr.szul@csiro.au>
2016-09-01 14:16:55 +1000

    Removed a rouge version of java sparkle

--- java/au/csiro/sparkle/common/args4j/RegisterHandlers.java
+++
@@ -1,8 +0,0 @@
-package au.csiro.sparkle.common.args4j;
-
-import java.util.Date;
-
-import org.kohsuke.args4j.CmdLineParser;
-
-public class RegisterHandlers {
-}
--- java/au/csiro/sparkle/spark/SparkTask.java
+++
@@ -1,6 +0,0 @@
-package au.csiro.sparkle.spark;
-
-
-public interface SparkTask {
-	SparkTask run(AppContext ac);
-}
