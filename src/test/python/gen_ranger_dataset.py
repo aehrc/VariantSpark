597961c80c6f2ad415e8d547d537f6dd811f620f
Piotr Szul <piotr.szul@csiro.au>
2016-09-01 11:34:10 +1000

    Added generation of traditional format to ranger data

--- src/test/python/gen_ranger_dataset.py
+++ src/test/python/gen_ranger_dataset.py
@@ -1,31 +1,24 @@
 import numpy as np
-np.random.randint(0, 3, (10, 10), np.int8)
-v = np.random.randint(0, 3, (150000, 10000), np.int8)
-v
 import pandas as pd
-pd.DataFrame(v)
-dv = pd.DataFrame(v)
-dv.to_csv('ranger.csv')
 from scipy.special import expit
-expit(0)
-v * np.zeros(150000)
-v * np.zeros(150000).t
-np.zeros(150000)
-np.dot(v, np.zeros(150000))
-v[0:5]
-v[0:5].shalw
-v[0:5].shape
-np.transpose(v[0:5])
-np.transpose(v[0:5]) * np.ones(5)
-np.dot(np.transpose(v[0:5]),np.ones(5))
-np.dot((np.transpose(v[0:5])-1),np.ones(5))
-np.dot((np.transpose(v[0:5])-1),2*np.ones(5))
-expit(np.dot((np.transpose(v[0:5])-1),2*np.ones(5)))
-expit(np.dot((np.transpose(v[0:5])-1),2*np.ones(5)))
-expit(np.dot((np.transpose(v[0:5])-1),2*np.ones(5))) >0.5
-resp = expit(np.dot((np.transpose(v[0:5])-1),2*np.ones(5))) >0.5
-resp.astype(np.int)
-respi = resp.astype(np.int)
-resp_d = pd.DataFrame(dict(resp5=respi))
-resp_d
-resp_d.to_csv('labels.csv', index = False)
+import os
+
+BASEDIR =  os.path.abspath(os.path.join(os.path.basename(__file__), '../../../..'))
+def proj_path(path):
+    return os.path.join(BASEDIR, path)
+
+n_samples = 10000
+n_variables = 150000
+
+np.random.seed(13)
+data = np.random.randint(0, 3, (n_variables, n_samples), np.int8)
+data_df = pd.DataFrame(data)
+#data_df.to_csv(proj_path("tmp/ranger-wide_%s_%s.csv" % (n_variables, n_samples)))
+resp = expit(np.dot((np.transpose(data[0:5])-1),2*np.ones(5))) >0.5
+resp_df = pd.DataFrame(dict(resp5=resp.astype(np.int)))
+resp_df.to_csv(proj_path("tmp/ranger-labels_%s_%s.csv" % (n_variables, n_samples)), index = True)
+
+data_narrow = np.transpose(data)
+narrow_df = pd.DataFrame(data_narrow)
+narrow_df["resp5"] = resp.astype(np.int)
+narrow_df.to_csv(proj_path("tmp/ranger_%s_%s.csv" % (n_variables, n_samples)), index = True)
