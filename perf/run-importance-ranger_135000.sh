b802a0739683065a616629eebe27075a85ebb139
Piotr Szul <piotr.szul@gmail.com>
2016-09-02 13:05:55 +1000

    Added timinign to Importance command; fixed scripts

--- perf/run-importance-ranger_135000.sh
+++ perf/run-importance-ranger_135000.sh
@@ -9,4 +9,4 @@ DATA_INPUT_DIR=${DATA_DIR}/input
 
 export VS_ECHO_CMDLINE=YES
 
-variant-spark --spark --master yarn-client --num-executors 128 --executor-memory 2G --driver-memory 4G --conf spark.serializer=org.apache.spark.serializer.KryoSerializer -- importance -if "${DATA_INPUT_DIR}/ranger_150000_10000.csv.bz2" -it csv -ff "${DATA_INPUT_DIR}/ranger-labels_10000.csv" -fc resp5 -v -sp 128 -rt 135000 -t 10 
+variant-spark --spark --master yarn-client --num-executors 32 --executor-memory 2G --driver-memory 4G --conf spark.serializer=org.apache.spark.serializer.KryoSerializer -- importance -if "${DATA_INPUT_DIR}/ranger-wide_150000_10000.csv.bz2" -it csv -ff "${DATA_INPUT_DIR}/ranger-labels_10000.csv" -fc resp5 -v -sp 32 -rt 135000 -t 10 
