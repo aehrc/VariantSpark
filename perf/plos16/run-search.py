f0d29777d1b3426cd248e702e00f094f2a4e7479
Piotr Szul <piotr.szul@csiro.au>
2016-09-20 17:14:12 +1000

    get-data workint

--- perf/plos16/run-search.py
+++ perf/plos16/run-search.py
@@ -20,11 +20,16 @@ DEF_SPARK_OPTIONS = {
 }
 
 @click.group()
-def cmd():
+@click.option('--local', required = False, is_flag=True)
+@click.pass_context
+def cmd(ctx, **kwargs):
+    ctx.obj = kwargs
     pass
 
-def run_variant_spark(cmd, args, spark_args=DEF_SPARK_OPTIONS, output = None, local=True):
-    spark_runner =  "--local --" if local else "--spark %s --" % " ".join(map(" ".join,spark_args.items()))
+@click.pass_context
+def run_variant_spark(ctx, cmd, args, spark_args=DEF_SPARK_OPTIONS, output = None):
+    print("Context: %s" % ctx.obj)
+    spark_runner =  "--local --" if ctx.obj['local'] else "--spark %s --" % " ".join(map(" ".join,spark_args.items()))
     vs_cmd = path.join(BASE_DIR, 'variant-spark')
     cmd_line = "%s %s  %s %s" % (vs_cmd, spark_runner, cmd ,  " ".join(map(" ".join,args.items())))
     print("Running: %s" % cmd_line)
@@ -33,25 +38,24 @@ def run_variant_spark(cmd, args, spark_args=DEF_SPARK_OPTIONS, output = None, lo
         exit_code = subprocess.call(shlex.split(cmd_line), stdout = outfile, stderr = subprocess.STDOUT)
     print("Exit code: %s" % exit_code)
     
-def run_gen_data(output_dir, local, nvars, nsamples):
+def run_gen_data(output_dir, nvars, nsamples):
     run_variant_spark('gen-features', {
                 '--gen-n-samples':nsamples,
                 '--gen-n-variables': nvars, 
                 '--spark-par':'256',
                 '--seed':'13',
                 '--output-file':path.join(output_dir, "data_s%s_v%s.parquet" %(nsamples, nvars))
-                }, output = path.join(output_dir, "data_s%s_v%s.out" %(nsamples, nvars)), local=local)
+                }, output = path.join(output_dir, "data_s%s_v%s.out" %(nsamples, nvars)))
 
 
 @cmd.command()
 @click.option('--nvars', '-v', multiple=True, required = True)
 @click.option('--nsamples', '-s', multiple=True, required = True)
 @click.option('--output-dir', required = True)
-@click.option('--local', required = False, is_flag=True)
-def gen_data(output_dir, local, **kwargs):
+def gen_data(output_dir, **kwargs):
     search_grid = ParameterGrid(kwargs)
     for args in search_grid:
-        run_gen_data(output_dir = output_dir, local=local, **args)
+        run_gen_data(output_dir = output_dir, **args)
 
 @cmd.command()
 @click.option('--input', '-i', multiple=True)
