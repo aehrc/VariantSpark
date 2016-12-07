352daa66a3391094fb18a7a5cf2bf8057b6a6522
Piotr Szul <piotr.szul@csiro.au>
2016-12-07 13:49:56 +1000

    Removed unnecessary output from tests

--- src/test/scala/au/csiro/variantspark/input/generate/NoisyEfectLabelGeneratorTest.scala
+++ src/test/scala/au/csiro/variantspark/input/generate/NoisyEfectLabelGeneratorTest.scala
@@ -13,7 +13,6 @@ class NoisyEfectLabelGeneratorTest extends SparkTest {
   def testResponseGenertion_var_0_2_prec_0_75() {
     
     val freatureGenerator = OrdinalFeatureGenerator(3, 1000, 500)
-    freatureGenerator.features().take(10).foreach(f => println(f.label, f.values.toList))    
     val labelGenerator  = new NoisyEfectLabelGenerator(freatureGenerator)(1, Map("v_0"-> 1.0, "v_1"-> 0.5, "v_2"-> 0.25), fractionVarianceExplained = 0.2, classThresholdPrecentile = 0.75)
     val classes = labelGenerator.getLabels(freatureGenerator.sampleNames)
     // we wouild expect 75% of samples in class 0
@@ -30,7 +29,6 @@ class NoisyEfectLabelGeneratorTest extends SparkTest {
   def testResponseGenertion_var_0_5_prec_0_50() {
     
     val freatureGenerator = OrdinalFeatureGenerator(3, 1000, 500)
-    freatureGenerator.features().take(10).foreach(f => println(f.label, f.values.toList))    
     val labelGenerator  = new NoisyEfectLabelGenerator(freatureGenerator)(1, Map("v_0"-> 1.0, "v_1"-> 0.5, "v_2"-> 0.25), fractionVarianceExplained = 0.5, classThresholdPrecentile = 0.5)
     val classes = labelGenerator.getLabels(freatureGenerator.sampleNames)
     // we wouild expect 75% of samples in class 0
