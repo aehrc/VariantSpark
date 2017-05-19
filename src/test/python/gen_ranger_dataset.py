0767c323981728f286a5acfcd4e1e4463b7000fb
Lynn Langit <lynnlangit@live.com>
2017-05-18 22:32:42 -0700

    remove untracked

--- src/test/python/gen_ranger_dataset.py
+++ src/test/python/gen_ranger_dataset.py
@@ -3,9 +3,12 @@ import pandas as pd
 from scipy.special import expit
 import os
 
-BASEDIR =  os.path.abspath(os.path.join(os.path.basename(__file__), '../../../..'))
+BASEDIR = os.path.abspath(os.path.join(os.path.basename(__file__), '../../../..'))
+
+
 def proj_path(path):
     return os.path.join(BASEDIR, path)
+
 
 n_samples = 10000
 n_variables = 150000
@@ -13,12 +16,12 @@ n_variables = 150000
 np.random.seed(13)
 data = np.random.randint(0, 3, (n_variables, n_samples), np.int8)
 data_df = pd.DataFrame(data)
-#data_df.to_csv(proj_path("tmp/ranger-wide_%s_%s.csv" % (n_variables, n_samples)))
-resp = expit(np.dot((np.transpose(data[0:5])-1),2*np.ones(5))) >0.5
+# data_df.to_csv(proj_path("tmp/ranger-wide_%s_%s.csv" % (n_variables, n_samples)))
+resp = expit(np.dot((np.transpose(data[0:5]) - 1), 2 * np.ones(5))) > 0.5
 resp_df = pd.DataFrame(dict(resp5=resp.astype(np.int)))
-resp_df.to_csv(proj_path("tmp/ranger-labels_%s_%s.csv" % (n_variables, n_samples)), index = True)
+resp_df.to_csv(proj_path("tmp/ranger-labels_%s_%s.csv" % (n_variables, n_samples)), index=True)
 
 data_narrow = np.transpose(data)
 narrow_df = pd.DataFrame(data_narrow)
 narrow_df["resp5"] = resp.astype(np.int)
-narrow_df.to_csv(proj_path("tmp/ranger_%s_%s.csv" % (n_variables, n_samples)), index = True)
+narrow_df.to_csv(proj_path("tmp/ranger_%s_%s.csv" % (n_variables, n_samples)), index=True)
