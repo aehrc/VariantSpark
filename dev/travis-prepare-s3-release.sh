3348d720cfc4e5d103ead1948c4fc71906e15b18
Piotr Szul <piotr.szul@csiro.au>
2017-09-12 16:32:38 +1000

    Added build number:

--- dev/travis-prepare-s3-release.sh
+++ dev/travis-prepare-s3-release.sh
@@ -13,7 +13,9 @@ COMMIT_TAG=${TRAVIS_COMMIT:-unknown}
 echo "Version: ${PROJECT_VERSION}-${COMMIT_TAG}"
 
 rm -rf target/s3-release
-mkdir -p target/s3-release
-mkdir -p target/s3-release/lib
 
-cp target/variant-spark_*.jar target/s3-release/lib
+S3_RELASE_DIR=target/s3-release/${PROJECT_VERSION}/${COMMIT_TAG}
+mkdir -p ${S3_RELASE_DIR}
+mkdir -p ${S3_RELASE_DIR}/lib
+
+cp target/variant-spark_*.jar ${S3_RELASE_DIR}/lib
