2096c4232c73838d0b66ca3e0a25e5ff97fde097
Piotr Szul <piotr.szul@csiro.au>
2017-09-14 11:52:14 +1000

    [CD] Added deployment of bootstrap scripts wqin s3 release

--- dev/travis-prepare-s3-release.sh
+++ dev/travis-prepare-s3-release.sh
@@ -12,11 +12,11 @@ if [[ -n "${CI_TAG}" ]]; then
 	echo "Tagged build for tag: ${CI_TAG}"
 	#TODO: Add the check that the CI_VERSION is the same as TAGGED"
  	S3_VERSION="${CI_VERSION}"	
-	S3_BASE_DIR=release
+	S3_BASE_DIR=stable
 else
         echo "Commit build for hash: ${CI_COMMIT_HASH_SHORT}"
 	S3_VERSION="${CI_BASE_VERSION}-${CI_COMMIT_HASH_SHORT}"
-        S3_BASE_DIR=builds
+        S3_BASE_DIR=unstable
 fi
 
 S3_VERSION_DIR="${S3_BASE_DIR}/${CI_BASE_VERSION}"
@@ -36,3 +36,8 @@ echo "${S3_VERSION}" | tee  "${S3_BUILD_VERSION_DIR}/latest"
 mkdir -p "${S3_BUILD_RELEASE_DIR}/lib"
 cp target/buildinfo "${S3_BUILD_RELEASE_DIR}/buildinfo"
 cp target/variant-spark_*.jar "${S3_BUILD_RELEASE_DIR}/lib"
+
+# deploy boostrap scripts 
+mkdir -p "${S3_BUILD_RELEASE_DIR}/bootstrap"
+cp cloud/aws-emr/bootstrap/* "${S3_BUILD_RELEASE_DIR}/bootstrap" 
+
