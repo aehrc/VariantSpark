cdd1c10754eb9191af83f5ee7ae8754a0ceeaff3
Prabha Pillay <ppillay@dius.com.au>
2017-12-14 09:26:02 +1100

    Missed file

--- python/variants/tests/tests.py
+++ python/variants/tests/tests.py
@@ -1,24 +1,22 @@
+import unittest
+
 from pyspark.sql import SparkSession
-from pyspark.tests import PySparkTestCase
+from pyspark.tests import ReusedPySparkTestCase
+
 from variants import VariantsContext
 
-class SparkSessionTestCase(PySparkTestCase):
-    @classmethod
-    def setUpClass(cls):
-        PySparkTestCase.setUpClass()
-        cls.spark = SparkSession(cls.sc)
 
-    @classmethod
-    def tearDownClass(cls):
-        PySparkTestCase.tearDownClass()
-        cls.spark.stop()
-
-class SampleTestWithPySparkTestCase(SparkSessionTestCase):
+class SampleTestWithPySparkTestCase(ReusedPySparkTestCase):
     def test_variants_context(self):
+        self.spark = SparkSession(self.sc)
         vc = VariantsContext(self.spark)
-        label = vc.load_label(label_file_path="../data/chr22-labels.csv", col_name="22_16050408")
-        features = vc.import_vcf(vcf_file_path="../data/chr22_1000.vcf")
+
+        label = vc.load_label(label_file_path="data/chr22-labels.csv", col_name="22_16050408")
+        features = vc.import_vcf(vcf_file_path="data/chr22_1000.vcf")
         imp_analysis = features.importance_analysis(label, 100, float('nan'), True, 13, 100, 3)
         imp_vars = imp_analysis.important_variables(20)
-        most_imp = imp_vars.head()._1()
-        self.assertEqual(most_imp, 'abc')
+        most_imp_var = imp_vars.head()._1()
+        self.assertEqual(most_imp_var, '22_16050408')
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
