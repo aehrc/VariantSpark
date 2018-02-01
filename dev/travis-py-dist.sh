fe0aaa1a2940f67aeaa90a356a9b9b77d4a38166
Piotr Szul <piotr.szul@csiro.au>
2018-02-01 12:48:14 +0100

    Added uploading to pypi

--- dev/travis-py-dist.sh
+++ dev/travis-py-dist.sh
@@ -12,6 +12,6 @@ cd "$FWDIR"
 . ${FWDIR}/dev/travis-set-ver.sh
 
 pushd  ${FWDIR}/python
-python setup.py egg_info -b "${CI_PYTHON_TAG}" setup.py sdist
+python setup.py egg_info -b "${CI_PYTHON_TAG}" sdist
 popd 
 
