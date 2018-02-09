362a4c0662a099583083b977dd03283a8145257b
Piotr Szul <piotr.szul@csiro.au>
2018-02-09 10:35:22 +0100

    Fixed unit tests to compile

--- src/test/scala/au/csiro/variantspark/pedigree/impl/RecombinationMapTest.scala
+++ src/test/scala/au/csiro/variantspark/pedigree/impl/RecombinationMapTest.scala
@@ -5,24 +5,10 @@ import org.junit.Assert._
 import org.junit.Test
 import it.unimi.dsi.util.XorShift1024StarRandomGenerator
 
-import org.json4s._
-import org.json4s.native.Serialization
-import org.json4s.native.Serialization.{read, write}
-
 class RecombinationMapTest {
   
   @Test
   def testLoadBedFile() {
     val rm = RecombinationMap.fromBedFile("tmp/HapMap/hg19/genetic_map_GRCh37.bed.gz")
-    println(rm)
-    val rng = new XorShift1024StarRandomGenerator(13L)
-   
-    implicit val formats = Serialization.formats(NoTypeHints)
-
-    for (i <- 0 until 10) {    
-      val gameteSpec = HapMapGameteSpecFactory(rm).createHomozigoteSpec()
-      val ser = write(gameteSpec)
-      println(ser)
-    }
   }
 }
