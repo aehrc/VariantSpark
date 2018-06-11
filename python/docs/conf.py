d8cc78f6322ba53faf6ef5519d5a0ffc2de7428d
piotrszul <piotr.szul@gmail.com>
2018-06-11 11:55:03 +1000

    Fixing  #52 (PyDocs strings for read the docs (#77)
    * Changed docs theme to readthedocs
    
    * Added ReadTheDocs requirements file to point to python code location
    
    * Added ReadTheDocs requirements file to point to python code location
    
    * Moved configuring python path for sphinx into conf.py to work with read the docs
    
    * Added read the docs requiremnts to compensate for not installing dependencies with setup.py
    
    * Fixed dependencies for read the docs build

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -15,6 +15,16 @@
 import sys
 import os
 
+
+# add python sources and dependecies to the list
+# This is need for read the docs to pickup the sources without instaalling the package.
+# The installation is not possible becasue jar file is missing ...
+# TODO: Consider a JAR less setup ???
+import glob
+PYTHON_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+sys.path.append(PYTHON_ROOT)
+sys.path+=glob.glob(os.path.join(PYTHON_ROOT, 'deps', '*.zip'))
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -111,7 +121,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
