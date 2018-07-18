2f5557cac0f1bd3dfe125789c80fbc1be8b78869
Piotr Szul <piotr.szul@gmail.com>
2018-07-18 14:35:08 +1000

    Updated private examples to use environment data root

--- private/local_run-null-importance-hipster.sh
+++ private/local_run-null-importance-hipster.sh
@@ -3,10 +3,12 @@
 PWD=$(cd `dirname "$0"`/..; pwd)
 PATH=${PATH}:${PWD}/bin
 export VS_ECHO_CMDLINE=YES
+DATA_ROOT="${VS_DATA_ROOT:-$PWD}"
 
-variant-spark --spark --master 'local[*]' -- \
- null-importance -if ${PWD}/tmp/hipster.vcf  -ff ${PWD}/data/hipsterIndex/hipster_labels.txt -fc label -v -rn 1000 -rbs 100  -sr 13 \
- -of ${PWD}/tmp/null_imp_hipster.csv \
+
+variant-spark --spark --master 'local[*]' --driver-memory 20G -- \
+ null-importance -if ${DATA_ROOT}/data/hipsterIndex/hipster.vcf  -ff ${DATA_ROOT}/data/hipsterIndex/hipster_labels.txt -fc label -v -rn 1000 -rbs 200  -sr 13 \
+ -of ${DATA_ROOT}/tmp/null_imp_hipster.csv \
  -pn 50  -ovn raw -oc 1 \
  "$@"
 
