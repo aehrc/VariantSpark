5c10e95c73afc677c046c90e6371fb288e3407a9
Piotr Szul <piotr.szul@gmail.com>
2018-07-25 14:54:11 +1000

    Updated private scrips for running permutations on hipster index

--- private/local_run-null-importance-hipster.sh
+++ private/local_run-null-importance-hipster.sh
@@ -5,10 +5,12 @@ PATH=${PATH}:${PWD}/bin
 export VS_ECHO_CMDLINE=YES
 DATA_ROOT="${VS_DATA_ROOT:-$PWD}"
 
+NO_TREES=2000
+NO_PERM=100
 
 variant-spark --spark --master 'local[*]' --driver-memory 20G -- \
- null-importance -if ${DATA_ROOT}/data/hipsterIndex/hipster.vcf  -ff ${DATA_ROOT}/data/hipsterIndex/hipster_labels.txt -fc label -v -rn 1000 -rbs 200  -sr 13 \
- -of ${DATA_ROOT}/tmp/null_imp_hipster.csv \
- -pn 50  -ovn raw -oc 1 \
+ null-importance -if ${DATA_ROOT}/data/hipsterIndex/hipster.vcf  -ff ${DATA_ROOT}/data/hipsterIndex/hipster_labels.txt -fc label -v -rn $NO_TREES -rbs 200  -sr 13 \
+ -of ${DATA_ROOT}/tmp/null_imp_hipster_${NO_TREES}_${NO_PERM}.csv \
+ -pn $NO_PERM  -ovn raw -oc 1 \
  "$@"
 
