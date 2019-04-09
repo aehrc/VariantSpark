59934360f827e0365f4c4f0fbc96ff9bb806b322
Piotr Szul <piotr.szul@csiro.au>
2019-04-09 14:38:43 +1000

    Clean up: removed all splitting classes

--- src/main/java/au/csiro/variantspark/algo/ClassificationSplitter.java
+++
@@ -1,7 +0,0 @@
-package au.csiro.variantspark.algo;
-
-public interface ClassificationSplitter {
-	SplitInfo findSplit(double[] data, int[] splitIndices);
-	SplitInfo findSplit(int[] data, int[] splitIndices);
-	SplitInfo findSplit(byte[] data, int[] splitIndices);
-}
