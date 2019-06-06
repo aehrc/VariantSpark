0ceff6ec2029cabe842de5422c75de0f0f2019b9
Piotr Szul <piotr.szul@csiro.au>
2019-06-06 17:26:05 +0200

    Making release 0.2.0-b1

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@ TEMP_PATH = "target"
 
 in_src = os.path.isfile(os.path.join(ROOT_DIR, "pom.xml"))
         
-VERSION = '0.2.0a3.dev0'
+VERSION = '0.2.0b1'
     
 # Provide guidance about how to use setup.py
 incorrect_invocation_message = """
