7fbb9aaf92e6e389bf1953f34c92d08202a38828
Piotr Szul <piotr.szul@csiro.au>
2020-04-09 17:11:52 +1000

    Added manual scalafmt.sh

---
+++ dev/scalafmt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -x
+
+FWDIR="$(cd "`dirname $0`"/..; pwd)"
+cd "$FWDIR"
+
+VERSION="${@:-2.11}"
+mvn mvn-scalafmt_$VERSION:format -Dscalafmt.skip=false -Dscalafmt.validateOnly=false
