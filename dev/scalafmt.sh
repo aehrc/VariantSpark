fa7a88d907c8ab010231f19c80fa5f3307ff30b5
Piotr Szul <piotr.szul@csiro.au>
2020-04-09 21:30:12 +1000

    Added info on formattintg to contibuting

--- dev/scalafmt.sh
+++ dev/scalafmt.sh
@@ -7,4 +7,5 @@ FWDIR="$(cd "`dirname $0`"/..; pwd)"
 cd "$FWDIR"
 
 VERSION="${@:-2.11}"
-mvn mvn-scalafmt_$VERSION:format -Dscalafmt.skip=false -Dscalafmt.validateOnly=false
+mvn mvn-scalafmt_$VERSION:format -Dscalafmt.skip=false -Dscalafmt.validateOnly=false \
+  -Dscalafmt.onlyChangedFiles=false
