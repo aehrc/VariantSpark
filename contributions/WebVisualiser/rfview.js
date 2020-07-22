0a46944e39ed9a77f13e69d1f3be3b297fd38e95
ArashBayatDev <Arash.Bayat@csiro.au>
2020-07-22 16:23:20 +1000

    Add Limits

--- contributions/WebVisualiser/rfview.js
+++ contributions/WebVisualiser/rfview.js
@@ -10,7 +10,7 @@ function destroy() {
 }
 
 var nodeId = 0;
-function DSF(node, parent, level, nodes, edges) {
+function DSF(node, parent, level, nodes, edges, limits) {
   // Assign new node id (integer)
   // nodeId is a global variable used in a recursive function
   nodeId++;
@@ -39,12 +39,24 @@ function DSF(node, parent, level, nodes, edges) {
   // increase tree deppth befor calling the function (recursively) for the child node
   level++;
 
+  // update limits
+  if (limits.samples.min > visjsNode.size) limits.samples.min = visjsNode.size;
+  if (limits.samples.max < visjsNode.size) limits.samples.max = visjsNode.size;
+  if (limits.impurity.min > visjsNode.impurity)
+    limits.impurity.min = visjsNode.impurity;
+  if (limits.impurity.max < visjsNode.impurity)
+    limits.impurity.max = visjsNode.impurity;
+  if (limits.impurityReduction.min > visjsNode.impurityReduction)
+    limits.impurityReduction.min = visjsNode.impurityReduction;
+  if (limits.impurityReduction.max < visjsNode.impurityReduction)
+    limits.impurityReduction.max = visjsNode.impurityReduction;
+
   // call the function for child node.
   if (node.right) {
-    DSF(node.right, thisNodeId, level, nodes, edges);
+    DSF(node.right, thisNodeId, level, nodes, edges, limits);
   }
   if (node.left) {
-    DSF(node.left, thisNodeId, level, nodes, edges);
+    DSF(node.left, thisNodeId, level, nodes, edges, limits);
   }
 }
 
@@ -54,9 +66,16 @@ function draw() {
   nodes = [];
   edges = [];
 
+  // Limits to generate color scale
+  var limits = {
+    samples: { min: 1000000000, max: 0 },
+    impurity: { min: 1, max: 0 },
+    impurityReduction: { min: 1, max: 0 },
+  };
   var treeIdToPlot = document.getElementById("treeId").value;
   nodeId = 0;
-  DSF(RF.trees[treeIdToPlot].rootNode, nodeId, 0, nodes, edges);
+  DSF(RF.trees[treeIdToPlot].rootNode, nodeId, 0, nodes, edges, limits);
+  console.log(limits);
 
   var data = {
     nodes: nodes,
