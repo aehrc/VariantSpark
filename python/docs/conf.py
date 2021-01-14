9648323bb93516ad6d323f6a59981d429ba453b5
piotrszul <piotr.szul@csiro.au>
2021-01-14 12:10:55 +1000

    Cleanup readthedocs setup. (#177)
    * Moving all dependencies for readthedocs to rtd-requirements.txt.
    
    * Added varspark sources to python path.
    
    * Added readthedocs configuration file.

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -15,16 +15,12 @@
 import sys
 import os
 
-
 # add python sources and dependecies to the list
 # This is need for read the docs to pickup the sources without instaalling the package.
 # The installation is not possible becasue jar file is missing ...
 # TODO: Consider a JAR less setup ???
-import glob
 PYTHON_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(PYTHON_ROOT)
-sys.path+=glob.glob(os.path.join(PYTHON_ROOT, 'lib', '*.zip'))
-sys.path+=glob.glob(os.path.join(PYTHON_ROOT, 'dev-lib', '*.zip'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
