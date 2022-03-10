2a8fa219d471df13aef3d491dcbc5c600665525f
Roc Reguant <roc.reguant@gmail.com>
2022-03-11 10:45:08 +1100

    piotrszul  from branch spike/193

--- src/test/scala/au/csiro/variantspark/hail/methods/RFModelHailTest.scala
+++ src/test/scala/au/csiro/variantspark/hail/methods/RFModelHailTest.scala
@@ -50,4 +50,41 @@ class RFModelHailTest {
     }
   }
 
+  /**
+   * The purpose of this test is basic valdation of the hail interface
+   * More substantial testing of the results is done from python
+   * The input vds for this test is created by:
+   *  `src/test/python/extract_rf_pheno_vds.py` called from `dev/test-gen-hail-cases.sh`
+   */
+  @Test
+  def testRunImportanceAnalysisWithCovriates() {
+
+    val matrixIR =
+      MatrixIR.read(sparkBackend.fs, "src/test/data/hail/chr22_1000-22_16050408-pheno.vds")
+
+    withResource(RFModel.pyApply(sparkBackend, matrixIR, null, true, null, null, 13, "mode")) {
+      rfModel =>
+        rfModel.fitTrees(100, 50)
+        assertTrue("OOB Error is defined", !rfModel.oobError.isNaN)
+        val importanceTableIR: TableIR = rfModel.variableImportance
+
+        assertEquals(List("locus", "alleles", "importance", "splitCount"),
+          importanceTableIR.typ.rowType.fieldNames.toList)
+
+        // collect the valued form the TableIR
+        // they cannot be directly converted to DataFrame as the contain
+        // non SparkSQL types such as Locus, which needs to be converted to structs
+        // before conversion to spark DataFame is possible
+        val collectedRows = ExecutionTimer.logTime("HailApiApp.collectValues") { timer =>
+          sparkBackend.withExecuteContext(timer) { ctx =>
+            val tv = Interpret(importanceTableIR, ctx, true)
+            tv.rvd.collect(ctx)
+          }
+        }
+        // Each row is [Locus, GenericArray, Double]
+        assertEquals("All variables have reported importance", 1988, collectedRows.size)
+    }
+  }
+
+
 }
