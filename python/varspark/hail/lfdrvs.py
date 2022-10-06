aa2c1efa0fe4e4296b22b75809e1f43950f8c077
Roc Reguant <roc.reguant@gmail.com>
2022-10-06 14:49:51 +1100

    adding automatic splitCount threshold selection function

--- python/varspark/hail/lfdrvs.py
+++ python/varspark/hail/lfdrvs.py
@@ -39,40 +39,72 @@ class LocalFdrVs:
         impTable = impTable.filter(impTable.splitCount >= 1)
         return LocalFdrVs.from_imp_df(impTable.to_spark(flatten=False).toPandas())
 
-    def plot_log_densities(self, ax, min_split_count=1, max_split_count=6, palette='Set1',
+
+    def find_split_count_th(self, cutoff_list=[1,2,3,4,5,10,15,20], quantile=0.75, bins=120):
+        """
+        Finds the ideal threshold for the splitCount. Ideal being the lowest differences between
+        the fitted skewed normal distribution vs the real data
+        :param cutoff_list: List of all values to be tried for the cutoff in the splitCount
+        :param quantile: Quantile to evaluate the distribution
+        :param bins: Number of bins for the distribution
+        :return: best splitCount threshold
+        """
+        best_split=[1, np.inf]
+
+        for split in cutoff_list:
+            #impDfWithLog = self.df_[self.df_.splitCount >= split]
+            impDfWithLog = self.df_[self.df_.splitCount >= split] #temp
+            impDfWithLog = impDfWithLog[['variant_id','logImportance']].set_index('variant_id').squeeze()
+
+            local_fdr = LocalFdr()
+            local_fdr.bins=bins
+
+
+            impDfWithLog = impDfWithLog + sys.float_info.epsilon
+            x, f_observed_y = local_fdr._observed_density(impDfWithLog)
+            f_y = local_fdr._fit_density(x, f_observed_y)
+
+            C = np.quantile(impDfWithLog, q=quantile)
+
+            initial_f0_params = local_fdr._estimate_skewnorm_params(x[x < C], f_observed_y[x < C], SkewnormParams.initial_list(impDfWithLog))
+
+            res = skewnorm.pdf(x, a=initial_f0_params.a, loc=initial_f0_params.loc, scale=initial_f0_params.scale) - f_observed_y
+            res = sum(res[x < C]**2)
+            if best_split[1] > res:
+                best_split[0]=split
+                best_split[1]=res
+
+        return best_split[0]
+
+    def plot_log_densities(self, ax, cutoff_list=[1,2,3,4,5,10,15,20], palette='Set1',
                            find_automatic_best=False, xLabel='log(importance)', yLabel='density'):
         """
         Plotting the log densities to visually identify the unimodal distributions.
         :param ax: Matplotlib axis as a canvas for this plot.
-        :param min_split_count: n>=1, from which the split count plotting starts.
-        :param max_split_count: when to stop the split count filtering.
+        :param cutoff_list: list of potential splitCount thresholds
         :param find_automatic_best: The user may let the computer highlight the potential best option.
         :param palette: Matplotlib color palette used for the plotting.
         :param xLabel: Label on the x-axis of the plot.
         :param yLabel: Label on the y-axis of the plot.
         """
-
-        assert min_split_count < max_split_count, 'min_split_count should be smaller than max_split_count'
-        assert min_split_count > 0, 'min_split_count should be bigger than 0'
         assert type(palette) == str, 'palette should be a string'
         assert type(xLabel) == str, 'xLabel should be a string'
         assert type(yLabel) == str, 'yLabel should be a string'
 
-        n_lines = max_split_count - min_split_count + 1
+        n_lines = len(cutoff_list)
         colors = sns.mpl_palette(palette, n_lines)
         df = self.df_
-        for i, c in zip(range(min_split_count, max_split_count + 1), colors):
+        for i, c in zip(cutoff_list, colors):
             sns.kdeplot(df.logImportance[df.splitCount >= i],
                         ax=ax, c=c, bw_adjust=0.5) #bw low show sharper distributions
 
         if find_automatic_best:
-            potential_best = self.find_split_count_th( min_split_count, max_split_count)
+            potential_best = self.find_split_count_th(cutoff_list=cutoff_list)
             sns.kdeplot(df.logImportance[df.splitCount >= potential_best],
                         ax = ax, c=colors[potential_best-1], bw_adjust=0.5, lw=8, linestyle=':')
-            best_split = [str(x) if x != potential_best else str(x)+'*' for x in range(
-                min_split_count, max_split_count+1)]
+            best_split = [str(x) if x != potential_best else str(x)+'*' for x in cutoff_list]
         else:
-            best_split = list(range(min_split_count, max_split_count+1))
+            best_split = cutoff_list
 
         ax.legend(title='Minimum split counts in distribution')
         ax.legend(labels=best_split, bbox_to_anchor=(1,1))
