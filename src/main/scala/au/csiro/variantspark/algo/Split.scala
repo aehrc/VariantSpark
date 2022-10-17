b192cceaf9a84e3f8c52e6a61e3ed8f7c6f8b01d
Roc Reguant <roc.reguant@gmail.com>
2022-10-17 12:42:41 +1100

    add regressionImpurity splitter and correcting class typo

--- src/main/scala/au/csiro/variantspark/algo/Split.scala
+++ src/main/scala/au/csiro/variantspark/algo/Split.scala
@@ -14,7 +14,7 @@ case class SplitInfo(splitPoint: Double, gini: Double, leftGini: Double, rightGi
 
 /**
   * An aggregator for calculating split impurity for two sets of labels or values
-  * indireclty referenced by theid indexes.
+  * indirectly referenced by their indexes.
   */
 trait IndexedSplitAggregator {
   def left: ImpurityAggregator
@@ -70,7 +70,7 @@ class ClassificationSplitAggregator private (val labels: Array[Int],
 }
 
 object ClassificationSplitAggregator {
-  def apply(impurity: ClassficationImpurity, labels: Array[Int],
+  def apply(impurity: ClassificationImpurity, labels: Array[Int],
       nCategories: Int): ClassificationSplitAggregator =
     new ClassificationSplitAggregator(labels, impurity.createAggregator(nCategories),
       impurity.createAggregator(nCategories))
@@ -83,7 +83,7 @@ object ClassificationSplitAggregator {
 class ConfusionAggregator private (val matrix: Array[ClassificationImpurityAggregator],
     val labels: Array[Int]) {
 
-  def this(impurity: ClassficationImpurity, size: Int, nCategories: Int, labels: Array[Int]) {
+  def this(impurity: ClassificationImpurity, size: Int, nCategories: Int, labels: Array[Int]) {
     this(Array.fill(size)(impurity.createAggregator(nCategories)), labels)
   }
 
@@ -111,7 +111,7 @@ trait IndexedSplitter {
 }
 
 /**
-  * A helper trait for IndexedSplitter that select the actual implementaiton
+  * A helper trait for IndexedSplitter that select the actual implementation
   * base on the set of indexes themselves.
   */
 trait SwitchingIndexedSplitter extends IndexedSplitter {
@@ -133,7 +133,7 @@ trait SplitterProvider {
 trait FastSplitterProvider extends SplitterProvider {
 
   /**
-    * The size of the required confusino aggregator
+    * The size of the required confusion aggregator
     */
   def confusionSize: Int
   def createSplitter(impCalc: IndexedSplitAggregator,
@@ -150,18 +150,18 @@ trait IndexedSplitterFactory {
 /**
   * Depending on weather the fast memory consuming splitter can be created
   * and the size of the current subset select either the fast memory consuming option
-  * slower but memory efficien one
+  * slower but memory efficient one
   * The way ranger does it is
   *  if (sampleSize/numOfUniqueValues < Q_THRESHOLD {
-  *    useSlowAlgorirm()
+  *    useSlowAlgorithm()
   *  else {
-  *   useFastAltorithm() if (available I assume)
+  *   useFastAlgorithm() if (available I assume)
   *
   *  The value of Q_THRESHOLD is 0.02
   */
 case class ThresholdIndexedSplitter(fastSplitter: IndexedSplitter, confusionSize: Int,
     defaultSplitter: IndexedSplitter,
-    qThreshold: Double = ThresholdIndexesSplitter.DefaultQThredhold)
+    qThreshold: Double = ThresholdIndexesSplitter.DefaultQThreshold)
     extends SwitchingIndexedSplitter {
 
   override def select(splitIndices: Array[Int]): IndexedSplitter = {
@@ -171,16 +171,16 @@ case class ThresholdIndexedSplitter(fastSplitter: IndexedSplitter, confusionSize
 }
 
 object ThresholdIndexesSplitter {
-  val DefaultQThredhold: Double = 0.02
+  val DefaultQThreshold: Double = 0.02
 }
 
 /**
   * The default implementation of the {{IndexedSplitterFactory}} for classification
   *
   */
-class DefStatefullIndexedSpliterFactory(val impurity: ClassficationImpurity,
+class DefStatefulIndexedSplitterFactory(val impurity: ClassificationImpurity,
     val labels: Array[Int], val nCategories: Int, val maxConfusionSize: Int = 10,
-    val qThreshold: Double = ThresholdIndexesSplitter.DefaultQThredhold)
+    val qThreshold: Double = ThresholdIndexesSplitter.DefaultQThreshold)
     extends IndexedSplitterFactory {
 
   lazy val splitAggregator: ClassificationSplitAggregator =
@@ -196,4 +196,46 @@ class DefStatefullIndexedSpliterFactory(val impurity: ClassficationImpurity,
       case _ => sf.createSplitter(splitAggregator)
     }
   }
+}
+
+/** REGRESSION SECTION */
+
+
+/**
+  * Split aggregator for classification. The indexes refer to nominal labels.
+  */
+class RegressionSplitterFactory(
+    val impurity: RegressionImpurity, val labels: Array[Float])
+    extends IndexedSplitterFactory {
+
+  lazy val splitAggregator: RegressionSplitAggregator =
+    RegressionSplitAggregator(impurity, labels)
+
+  def create(sf: SplitterProvider): IndexedSplitter = {
+    sf.createSplitter(splitAggregator)
+  }
+}
+
+class RegressionSplitAggregator private (val labels: Array[Float],
+    val left: RegressionImpurityAggregator, val right: RegressionImpurityAggregator)
+    extends IndexedSplitAggregator {
+
+  def initLabel(value: Double) {
+    right.addValue(value)
+  }
+
+  def updateLabel(value: Double) {
+    left.addValue(value)
+    right.subValue(value)
+  }
+
+  override def init(index: Int): Unit = initLabel(labels(index))
+
+  override def update(index: Int): Unit = updateLabel(labels(index))
+}
+
+object RegressionSplitAggregator {
+  def apply(impurity: RegressionImpurity, labels: Array[Float]): RegressionSplitAggregator =
+    new RegressionSplitAggregator(labels, impurity.createAggregator(),
+      impurity.createAggregator())
 }
