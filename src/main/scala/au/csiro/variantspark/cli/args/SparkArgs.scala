3289cd23ba22d770b0b5618d8abb17371ef2a8dd
Xu, Qinying (H&B, Herston) <Qinying.Xu@csiro.au>
2024-04-04 10:26:14 +1000

    use htsjdk.samtools.util.BlockCompressedInputStream to load .vcf.bgz

--- src/main/scala/au/csiro/variantspark/cli/args/SparkArgs.scala
+++ src/main/scala/au/csiro/variantspark/cli/args/SparkArgs.scala
@@ -3,6 +3,9 @@ package au.csiro.variantspark.cli.args
 import org.kohsuke.args4j.Option
 import au.csiro.pbdava.ssparkle.spark.SparkApp
 import org.apache.spark.rdd.RDD
+import htsjdk.samtools.util.BlockCompressedInputStream
+import org.apache.hadoop.fs.Path
+import java.io.File
 
 trait SparkArgs extends SparkApp {
 
@@ -10,7 +13,17 @@ trait SparkArgs extends SparkApp {
     aliases = Array("--spark-par"))
   val sparkPar: Int = 0
 
-  def textFile(inputFile: String): RDD[String] =
-    sc.textFile(inputFile, if (sparkPar > 0) sparkPar else sc.defaultParallelism)
-
+  def textFile(inputFile: String): RDD[String] = {
+    val input = new File(inputFile)
+    val isBGZ = input.getName.split('.').lastOption.getOrElse("").equalsIgnoreCase("bgz")
+    println(inputFile + " is loading to spark RDD " + isBGZ)
+    if (isBGZ) {
+      val path = new Path(inputFile)
+      val fs = path.getFileSystem(sc.hadoopConfiguration)
+      val bgzInputStream = new BlockCompressedInputStream(fs.open(path))
+      sc.parallelize(Stream.continually(bgzInputStream.readLine()).takeWhile(_ != null).toList)
+    } else {
+      sc.textFile(inputFile, if (sparkPar > 0) sparkPar else sc.defaultParallelism)
+    }
+  }
 }
