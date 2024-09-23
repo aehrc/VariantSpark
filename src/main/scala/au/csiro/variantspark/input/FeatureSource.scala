cef13f4f5516adf67135c2b130573b42a18fecca
NickEdwards7502 <nickedwards7502@gmail.com>
2024-09-23 14:15:09 +1000

    REFACTOR: Make head concrete & inherited by all feature sources (#237)

--- src/main/scala/au/csiro/variantspark/input/FeatureSource.scala
+++ src/main/scala/au/csiro/variantspark/input/FeatureSource.scala
@@ -2,6 +2,9 @@ package au.csiro.variantspark.input
 
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.types.{StringType, DoubleType, StructField, StructType}
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 import scala.reflect.ClassTag
 import au.csiro.variantspark.data.VariableType
 import au.csiro.variantspark.data.ContinuousVariable
@@ -11,4 +14,19 @@ import au.csiro.variantspark.data.Feature
 trait FeatureSource {
   def sampleNames: List[String]
   def features: RDD[Feature]
+
+  def head(sqlContext: SQLContext, rowLim: Int = 10, colLim: Int = 10): DataFrame = {
+    lazy val sampleNamesStructArr: Array[StructField] =
+      sampleNames.take(colLim).map(StructField(_, StringType, true)).toArray
+    lazy val featureDFSchema: StructType =
+      StructType(Seq(StructField("variant_id", StringType, true)) ++ sampleNamesStructArr)
+    val sc = sqlContext.sparkContext
+
+    val slicedFeatureArray: Array[Row] =
+      features.take(rowLim).map { f =>
+        Row.fromSeq(f.label +: f.valueAsStrings.take(colLim).toSeq)
+      }
+    val slicedFeatureRDD: RDD[Row] = sc.parallelize(slicedFeatureArray)
+    sqlContext.createDataFrame(slicedFeatureRDD, featureDFSchema)
+  }
 }
--- src/main/scala/au/csiro/variantspark/input/VCFFeatureSource.scala
+++ src/main/scala/au/csiro/variantspark/input/VCFFeatureSource.scala
@@ -7,9 +7,6 @@ import collection.JavaConverters._
 import au.csiro.variantspark.data.Feature
 import au.csiro.variantspark.data.BoundedOrdinalVariable
 import au.csiro.variantspark.data.StdFeature
-import org.apache.spark.sql.types.{StringType, ByteType, StructField, StructType}
-import org.apache.spark.SparkContext
-import org.apache.spark.sql.{DataFrame, Row, SQLContext}
 
 trait VariantToFeatureConverter {
   def convert(vc: VariantContext): Feature
@@ -59,22 +56,6 @@ class VCFFeatureSource(vcfSource: VCFSource, converter: VariantToFeatureConverte
     val converterRef = converter
     vcfSource.genotypes().map(converterRef.convert)
   }
-
-  def head(sqlContext: SQLContext, rowLim: Int = 10, colLim: Int = 10): DataFrame = {
-    lazy val sampleNamesStructArr: Array[StructField] =
-      sampleNames.take(colLim).map(StructField(_, ByteType, true)).toArray
-    lazy val featureDFSchema: StructType =
-      StructType(Seq(StructField("variant_id", StringType, true)) ++ sampleNamesStructArr)
-    val sc = sqlContext.sparkContext
-
-    val slicedFeatureArray: Array[Row] =
-      features.take(rowLim).map { f =>
-        Row.fromSeq(f.label +: f.valueAsByteArray.take(colLim).toSeq)
-      }
-    val slicedFeatureRDD: RDD[Row] = sc.parallelize(slicedFeatureArray)
-    sqlContext.createDataFrame(slicedFeatureRDD, featureDFSchema)
-  }
-
 }
 
 object VCFFeatureSource {
