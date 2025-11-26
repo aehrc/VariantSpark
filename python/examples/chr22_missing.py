3c5a4c5bf50fc03f76100d02e632aa7cccf9af69
NickEdwards7502 <nickedwards7502@gmail.com>
2025-11-26 13:45:18 +1000

    DOCS: Remove or update Python example scripts to use new API (#237)

---
+++ python/examples/chr22_missing.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+'''
+Created 4 Nov 2025
+
+@author: edw222 
+'''
+import os
+import varspark as vs
+from pyspark.sql import SparkSession
+
+PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+
+def main():
+    spark = SparkSession.builder.config('spark.jars', vs.find_jar()).getOrCreate()
+    vc = vs.VarsparkContext(spark)
+
+    data = vc.import_vcf(os.path.join(PROJECT_DIR, 'data/chr22_1000_missing.vcf'), imputation_strategy="mode")
+    labels = vc.load_label(os.path.join(PROJECT_DIR, 'data/chr22-labels.csv'), "22_16050408")
+
+    rf_model = vs.RandomForestModel(vc, seed=13, mtry_fraction=0.05, min_node_size=5, max_depth=10)
+    rf_model.fit_trees(data, labels, n_trees=100, batch_size=50)
+
+    print("OOB error: %s" % rf_model.oob_error())
+    ia = rf_model.importance_analysis()
+    print(ia.important_variables(limit=3).head())
+
+    rf_model.export_to_json(os.path.join(PROJECT_DIR, "target/chr22_1000_missing-model.json"), True)
+
+if __name__ == '__main__':
+    main()
