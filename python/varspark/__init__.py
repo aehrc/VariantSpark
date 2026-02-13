e972df0217eda8907ff9f546addc21ddfe4fa185
NickEdwards7502 <nickedwards7502@gmail.com>
2026-02-13 12:03:21 +1000

    FEAT: Add Kryo config helper to Python API * Add configure_spark() function to apply recommended Kryo settings for VariantSpark * Export configure_spark in varspark module

--- python/varspark/__init__.py
+++ python/varspark/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 try:
-    from varspark.core import VarsparkContext, VariantsContext
+    from varspark.core import VarsparkContext, VariantsContext, configure_spark
     from varspark.rfmodel import RandomForestModel, RFModelContext
 except Exception:
     if not os.environ.get('VS_FIND_JAR'):
